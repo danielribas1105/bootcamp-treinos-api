844a475f52d2a9a6be12272b556d1f1b9bf22151
daniel.ribas <daniel.ribas@isdbflowtech.com>
2026-02-26 23:53:35 -0300

    chore: add zod and fastify-type-provider-zod setup

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
-console.log("Hello World");
-export {};
+import "dotenv.config";
+import Fastify from "fastify";
+import { serializerCompiler, validatorCompiler, } from "fastify-type-provider-zod";
+const app = Fastify({
+    logger: true,
+});
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+// Declare a route
+app.get("/", async function handler(request, reply) {
+    return { hello: "world" };
+});
+// Run the server!
+try {
+    await app.listen({ port: Number(process.env.PORT) || 8081 });
+}
+catch (err) {
+    app.log.error(err);
+    process.exit(1);
+}
